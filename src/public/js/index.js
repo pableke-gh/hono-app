a37a310a9efa55893d9a7698d8e2ad56d1e24135
Pablo Rosique <pableke@gmail.com>
2024-12-10 18:03:18 +0100

    Anchoring css styles for popover

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -7,9 +7,9 @@ import i18n from "./i18n/langs.js";
 //const menuTree = menus.filter(node => (node.tipo == 1)).sort((a, b) => (a.orden - b.orden));
 
 nav.ready(() => {
-    const langs = document.getElementById("languages");
-    const link = langs.querySelector("a#" + i18n.get("lang")); // Language selector
-    langs.firstElementChild.firstElementChild.src = link.firstElementChild.src;
+    const langs = $1("[popovertarget='langs-menu']"); // Language button
+    const link = langs.nextElementSibling.querySelector("a#" + i18n.get("lang")); // Language selector
+    langs.firstElementChild.src = link.firstElementChild.src; // current flag
 
     const menuHTML = $1("ul.menu");
     //menuHTML.innerHTML = menuHTML.innerHTML || menu.html(menuTree);
--- src/public/js/modules/xeco/irse/tests.js
+++ src/public/js/modules/xeco/irse/tests.js
@@ -3,23 +3,21 @@ import nav from "../../../components/Navigation.js";
 import place from "./place.js";
 
 window.initMap = function() {
-	const OPTIONS = place.getOptions();
     const inputOrigen = $1("#ac-origen");
     const divAddress = $1(".address-components");
-    const origen = new google.maps.places.Autocomplete(inputOrigen, OPTIONS);
+
+	place.setAutocomplete(inputOrigen, origen => {
+        const placeMaps = origen.getPlace();
+        divAddress.render(place.toObject(placeMaps)).show();
+        console.log("CT =", place.isCartagena(placeMaps), placeMaps);
+	});
 
     const fnVisible = () => {
-		divAddress.setVisible(inputOrigen.value && origen.getPlace());
+		divAddress.setVisible(inputOrigen.value);
     }
     inputOrigen.focus();
     inputOrigen.addEventListener("search", fnVisible);
     inputOrigen.addEventListener("change", fnVisible);
-
-    origen.addListener("place_changed", function() {
-        const placeMaps = origen.getPlace();
-        divAddress.render(place.render(placeMaps)).show();
-        console.log("CT =", place.isCartagena(placeMaps), placeMaps);
-    });
 }
 
 // Register event on page load and export default handler
