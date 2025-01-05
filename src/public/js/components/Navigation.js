d46432b18ea6825c89c8d0c85a517989ceb56ecd
Pablo Rosique <pableke@gmail.com>
2025-01-05 11:21:18 +0100

    Improve responsive header font-size and padding

--- src/public/js/components/Navigation.js
+++ src/public/js/components/Navigation.js
@@ -35,14 +35,14 @@ function Navigation() {
 	this.runScript = (name, fn) => { fn(); return self.setScript(name, fn); } // Execute and save handler
 
 	// Capture clicks events to load main via AJAX
-	this.addClick = (el, selector) => {
+	/*this.addClick = (el, selector) => {
 		selector = selector || "a.load-main";
 		el.querySelectorAll(selector).addClick((ev, link) => {
 			api.init().text(link.href).then(self.setMain); // Load main via AJAX on click
 			ev.preventDefault();
 		});
 		return self;
-	}
+	}*/
 	this.setMain = data => {
 		if (!data) // exists changes
 			return self; // Not changes
@@ -50,45 +50,43 @@ function Navigation() {
 		main.innerHTML = data; // update contents
 		main.querySelectorAll("script[id]").forEach(script => {
 			const fn = self.getScript(script.id);
+			script.parentNode.removeChild(script);
 			if (fn) // Is function registered?
 				return fn(); // Simule dispatch vt event
+
+			// dynamic import
 			import(script.src).then(module => {
 				self.runScript(script.id, module.default);
 			}).catch(err => {
 				alerts.showError(err);
 			});
-			script.parentNode.removeChild(script);
 		});
 
-		alerts.top(); // Show top view
+		alerts.top(); // go to top view
 		tabs.load(main); // reload tabs events
-		return self.addClick(main); // Listen new clicks
+		return self;
 	}
 
 	// Check to see if API is supported
 	if (document.startViewTransition) {
-		// capture navigation event links
+		const RE_MAIN = /<main[^>]*>([\s\S]*)<\/main>/im;
+		let pathname = location.pathname; // current location
+		// capture all navigation event links
 		window.navigation.addEventListener("navigate", ev => {
 			const url = new URL(ev.destination.url);
 			//console.log(location.pathname, url, ev);
 			if (url.searchParams.get("nav") || self.isDynamic(url.pathname))
 				return; // Desactive View Transition interceptor
-			// Current location, Important! AJAX NOT to change url
-			if (location.pathname == url.pathname)
+			// Is current location? Important! AJAX NOT to change url
+			if (pathname == url.pathname)
 				return ev.preventDefault(); // Current destination
 			// Si es una pagina externa => ignoramos el evento
-			if (location.origin == url.origin) {
-				// Navegación en el mismo dominio (origin)
-				const RE_MAIN = /<main[^>]*>([\s\S]*)<\/main>/im;
-				const fnTransition = text => {
-					// Extraigo el contenido de la etiqueta main
-					const fnLoad = () => self.setMain(text.match(RE_MAIN)[1]);
-					// Utilizamos la api de View Transitions
-					document.startViewTransition(fnLoad);
-				}
-				// Intercept event => Cargamos la pagina de destino con fetch
-				const handler = () => api.init().text(url.href).then(fnTransition);
-				ev.intercept({ handler }); // intercept event
+			if (location.origin == url.origin) { // Navegación en el mismo dominio (origin)
+				const fnMain = text => self.setMain(text.match(RE_MAIN)[1]);
+				const fnLoad = () => api.init().text(url.href).then(fnMain);
+				document.startViewTransition(fnLoad); // set transition
+				pathname = url.pathname; // save current location
+				ev.preventDefault(); // avoid navigation
 			}
 		});
 	}
