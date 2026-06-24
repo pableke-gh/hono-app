d406ea36410043c57e58bc5a46dd111d2248b566
Pablo Rosique Vidal <pableke@gmail.com>
2026-06-24 15:29:06 +0200

    Primer commit para actualizar presto

--- src/public/js/presto/components/prestos.js
+++ src/public/js/presto/components/prestos.js
@@ -1,17 +1,55 @@
 
+import api from "../../core/components/Api.js";
+import tabs from "../../core/components/helper/Tabs.js";
 import i18n from "../i18n/langs.js";
-import presto from "../model/Presto.js";
-import firma from "../../core/model/Firma.js";
-import Solicitudes from "../../core/modules/solicitudes.js";
 
-export default class PrestoSolicitudes extends Solicitudes {
-	constructor() {
-		super(presto); // Must call super before 'this'
+import TableHTML from "../../core/components/Table.js";
+import firma from "../../core/model/Firma.js";
+import presto from "../model/Presto.js";
+
+export default class PrestosTable extends TableHTML {
+	showList = () => { this.reloadRow(); tabs.showList(); } // reload current row + show list tab
+	setWorking = () => { presto.setProcesando(); this.showList(); } // update current row state
+
+	connectedCallback() { // initialize table
+		const form = document.forms["presto-form"];
+		//form.setTable(this); // set table reference in form
+
+		// table actions
+		this.setMsgEmpty("No se han encontrado solicitudes para a la búsqueda seleccionada");
+		this.set("#emails", data => api.init().json("/uae/pedidos/emails?id=" + data.id)); // admin test email
+		this.set("#integrar", data => { // integra la solicitud seleccionada en uxxiec
+			const url = "/uae/pedidos/ws?id=" + presto.setData(data).getId();
+			i18n.confirm("msgIntegrar") && api.init().json(url).then(this.setWorking);
+		});
+
+		this.set("#view", form.load).set("#reject", form.reject).set("#report", this.report);
+		this.set("#firmar", data => { presto.setData(data); form.firmar.execute(); });
+		this.set("isFirmable", (link, data) => presto.setData(data).isFirmable())
+		this.set("isIntegrable", (link, data) => presto.setData(data).isIntegrable())
+		this.set("update-estado", (td, data) => { // actualizo la celda del estado
+			td.innerHTML = presto.setData(data).getDescEstado(); // set texto de estado
+			td.className = presto.getStyleByEstado() + " hide-xs table-reload"; // set estilos
+			return true; // show td cell
+		});
+
+		// buttons action
+		tabs.setAction("view", () => form.load(this.getCurrent()));
 	}
 
-	getSolicitud = () => presto; // get current instance
 	row(data) {
-		let acciones = super.row(data);
+		let acciones = '<a href="#view"><i class="fas fa-search action resize text-blue"></i></a>';
+		if (presto.setData(data).isFirmable()) { // initialize and verify state
+			acciones += '<a href="#firmar" class="resize table-refresh" data-refresh="isFirmable"><i class="fas fa-check action resize text-green"></i></a>';
+			acciones += '<a href="#reject" class="resize table-refresh" data-refresh="isFirmable"><i class="fas fa-times action resize text-red"></i></a>';
+		}
+		if (presto.isEjecutable())
+			acciones += '<a href="#uxxiec"><i class="fal fa-cog action resize text-green"></i></a>';
+		if (presto.isIntegrable())
+			acciones += '<a href="#integrar" class="table-refresh" data-refresh="isIntegrable"><i class="far fa-save action resize text-blue"></i></a>';
+		if (presto.isAdmin())
+			acciones += '<a href="#emails"><i class="fal fa-mail-bulk action resize text-blue"></i></a><a href="#remove"><i class="fal fa-trash-alt action resize text-red"></i></a>';
+
 		if (presto.isDocumentable()) {
 			//acciones += presto.isAdmin() ? '<a href="#pdf" title="Informe PRESTO"><i class="fas fa-file-pdf action resize text-red"></i></a>' : "";
 			acciones += '<a href="#report" title="Informe PRESTO"><i class="fal fa-file-pdf action resize text-red"></i></a>';
